507e326bc12a183c602b12d255826f0e7ca00ffd
git <dima.it.ua@gmail.com>
2023-03-03 18:50:08 +0200

    ok

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $name = htmlspecialchars($_POST['name']);
-  $mail  = htmlspecialchars($_POST['mail']);
-  $comment = htmlspecialchars($_POST['comment']);
-
-  echo  $name, $mail, $comment;
-?>
